44585d1ed5701e06372d13e200de4dcdef44c558
RahatVortex98 <r072islam@gmail.com>
2025-04-30 22:57:46 -0700

    Operators

---
+++ main.js
@@ -0,0 +1,133 @@
+// let name = 'Mosh';
+// let firstName = 'Mosh';
+// let lastName = 'Hamdani';
+
+// const age =30;
+
+
+// console.log(name);
+// console.log(firstName);
+// console.log(lastName);
+// console.log(age);
+
+
+// Arrays
+
+
+// let selectColors=['red','green'];
+//  console.log(selectColors);
+
+
+//  console.log(selectColors[0]);
+// selectColors[2]='black';
+//  console.log(selectColors.length)
+// console.log(selectColors)
+
+
+
+// functions
+
+// function greet(){
+//     console.log("Hello World");
+// }
+
+// greet();
+
+
+
+// function greet(name){
+//     console.log("Hello World " + name);
+// }
+
+// greet('mosh');
+
+// function greet(name,lastName){
+//     console.log("Hello World " + name + lastName);
+// }
+
+// greet('mosh','takla');
+
+
+
+// function square(number){
+//     return number*number;
+// }
+// console.log(square(2));+
+
+
+
+
+// ));
+
+
+// OPERATORS
+// Arthmetic Operators
+
+// let x = 10;
+// let y = 3;
+
+// console.log(x+y);
+// console.log(x-y);
+// console.log(x*y);
+// console.log(x/y);
+// console.log(x**y);
+
+// console.log(x++)
+// console.log(x)
+
+// console.log(++x);
+
+// console.log(--y);
+
+
+// Comparison Operators
+
+// x=10;
+
+// console.log(x>0);
+// console.log(x>=0);
+// console.log(x<0);
+// console.log(x===0);
+
+
+// console.log(1===1);
+// console.log('1'===1);
+
+// console.log(1==1);
+// console.log('1'==1);
+
+// Ternary Operators
+
+// let points = 110;
+// let type = points > 100 ? 'gold':'silver';
+// console.log(type);
+
+
+// swapping value
+
+
+// let a = 'red';
+// let b = 'blue';
+
+
+// let c = a;
+// a = b;
+// b = c;
+
+// console.log(a);
+// console.log(b);
+
+
+// ELSE IF
+
+
+// let hour = 10;
+
+// if (hour >=6 && hour < 12){
+//     console.log('Good Morning');
+// }
+// else if (hour >=12 && hour < 18){
+//     console.log('Good Afternoon');
+// }
+// else
+//     console.log('Good Evening');
